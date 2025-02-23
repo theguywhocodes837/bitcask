c9472f4a1259d27e412b0e23451d1966e2d1aa84
Vaibhav <vaibhav@Vaibhavs-MacBook-Pro.local>
2025-02-24 01:17:05 +0530

    Implement method to read the record at a given offset

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 	"sync"
 )
@@ -34,4 +36,57 @@ func Open(filename string) (*Store, error) {
 	}
 
 	return store, nil
+}
+
+func (s *Store) readRecord(offset int64) (*Record, error) {
+	_, err := s.file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return nil, fmt.Errorf("failed to seek: %w", err)
+	}
+
+	record := &Record{}
+
+	// read timestamp (4 bytes)
+	timeStampBuff := make([]byte, 4)
+	if _, err := s.file.Read(timeStampBuff); err != nil {
+		return nil, err
+	}
+	record.Timestamp = binary.BigEndian.Uint32(timeStampBuff)
+
+	// read key len (4 bytes)
+	keyLenBuff := make([]byte, 4)
+	if _, err := s.file.Read(keyLenBuff); err != nil {
+		return nil, err
+	}
+	keyLen := binary.BigEndian.Uint32(keyLenBuff)
+
+	// read value len (4 bytes)
+	valueLenBuff := make([]byte, 4)
+	if _, err := s.file.Read(valueLenBuff); err != nil {
+		return nil, err
+	}
+	valueLen := binary.BigEndian.Uint32(valueLenBuff)
+
+	// read TombStone (1 byte)
+	tombStoneBuff := make([]byte, 1)
+	if _, err := s.file.Read(tombStoneBuff); err != nil {
+		return nil, err
+	}
+	record.Tombstone = tombStoneBuff[0] == 1
+
+	// read key data
+	key := make([]byte, keyLen)
+	if _, err := s.file.Read(key); err != nil {
+		return nil, err
+	}
+	record.Key = key
+
+	// read value data
+	value := make([]byte, valueLen)
+	if _, err := s.file.Read(value); err != nil {
+		return nil, err
+	}
+	record.Value = value
+
+	return record, nil
 }
